03d495b0f8fe77b50620d50383895c2142b2f5a3
Toby Twigger <tobytwigger1@gmail.com>
2021-08-14 22:29:02 +0100

    Add loading of vue components as a plugin

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+
+export default {
+    install(Vue, options) {
+
+        const requireComponent = require.context(
+          // The relative path of the components folder
+          './components',
+          // Whether or not to look in subfolders
+          false,
+          // The regular expression used to match base component filenames. V*.vue
+          /V[A-Z]\w+\.(vue|js)$/
+        )
+
+        requireComponent.keys().forEach(fileName => {
+            // Get component config
+            const componentConfig = requireComponent(fileName)
+
+            // Get PascalCase name of component
+            const componentName = upperFirst(
+              camelCase(
+                // Gets the file name regardless of folder depth
+                fileName
+                  .split('/')
+                  .pop()
+                  .replace(/\.\w+$/, '')
+              )
+            )
+
+
+            // Register component globally
+            Vue.component(
+              // Swap the first letter from V to P
+              'P' + componentName.slice(1),
+              // Look for the component options on `.default`, which will
+              // exist if the component was exported with `export default`,
+              // otherwise fall back to module's root.
+              componentConfig.default || componentConfig
+            )
+        })
+
+    }
+}
