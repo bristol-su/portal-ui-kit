9c634a101cc15bfb946aa02b9a66bb41de602483
Toby Twigger <tobytwigger1@gmail.com>
2021-08-14 23:05:46 +0100

    Temporarily change the button title

--- src/components/atomic/VButton.vue
+++ src/components/atomic/VButton.vue
@@ -4,7 +4,7 @@
       type="submit"
       class="w-full px-16 py-2 my-2 mr-2 text-base text-white transition duration-500 ease-in-out transform bg-blue-600 border-blue-600 rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:bg-blue-800 "
   >
-    {{ title }}
+    {{ title }} - test
   </button>
 </template>
 
