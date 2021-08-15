0179d4b3a76b0994fe0f2b00b056c77542c86e58
Toby Twigger <tobytwigger1@gmail.com>
2021-08-15 17:54:44 +0100

    Fix file input being readonly

--- src/components/atomic/dynamic-form/VFileUpload.vue
+++ src/components/atomic/dynamic-form/VFileUpload.vue
@@ -8,7 +8,6 @@
             :name="name"
             @change="setValue($event.target.value)"
             tabindex="-1"
-            v-model="dynamicValue"
             type="file"
             class="w-full px-4 py-2 mt-2 text-base text-black transition duration-500 ease-in-out transform rounded-lg bg-blueGray-100 focus:border-blueGray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2">
       </div>
