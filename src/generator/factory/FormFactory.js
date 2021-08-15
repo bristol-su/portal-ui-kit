81d04032782116c4d65bb84adfee70b5a31542b2
Toby Twigger <tobytwigger1@gmail.com>
2021-08-16 00:18:46 +0100

    wip

--- src/generator/factory/FormFactory.js
+++ src/generator/factory/FormFactory.js
@@ -1,13 +1,30 @@
 import Form from '../schema/Form';
+import Group from '../schema/Group';
+import Field from '../schema/Field';
 
 export default class {
     fromJson(json) {
-        console.log(json);
         let form = new Form();
-        form.title(json.title);
-        form.subtitle(json.subtitle);
-        form.description(json.description);
+        form.title(json.form.title);
+        form.subtitle(json.form.subtitle);
+        form.description(json.form.description);
 
+        form.groups = json.form.groups.map(groupSchema => {
+            let group = new Group();
+            group.title( groupSchema.title);
+            group.subtitle(  groupSchema.subtitle);
+
+            let fields = groupSchema.fields.map(fieldSchema => {
+                let field = new Field(fieldSchema.type, fieldSchema.id);
+                field.schema = fieldSchema;
+                return field;
+            })
+
+            group.fields = fields;
+
+            return group;
+        })
+console.log(form);
         return form;
     }
 }
