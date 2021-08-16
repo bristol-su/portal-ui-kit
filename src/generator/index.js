bfa5be1bb8ff270aca1b58320ae1ed1a62a44506
Toby Twigger <tobytwigger1@gmail.com>
2021-08-16 02:45:40 +0100

    Update generator

--- src/generator/index.js
+++ src/generator/index.js
@@ -1,25 +1,4 @@
 import FormGenerator from './generators/FormGenerator';
-import GroupGenerator from './generators/GroupGenerator';
-import FieldGenerator from './generators/FieldGenerator';
-import FormFactory from './factory/FormFactory';
-
-
-
-let form = FormGenerator.newForm('Title', 'Subtitle', 'Description')
-  .withGroup(GroupGenerator.newGroup('Group Title', 'Group Subtitle')
-    .withField(
-      FieldGenerator.radios('radio_example')
-        .label('The radio label')
-        .required('The required value')
-        .tooltip('A tooltip')
-        .withOption('one', 'One')
-        .withOption('two', 'Two')
-        .withOption('three', 'Three')
-    ))
-  .generate();
-
-let factory = new FormFactory();
-factory.fromJson(form.asJson());
 
 export default {
     generator: new FormGenerator
