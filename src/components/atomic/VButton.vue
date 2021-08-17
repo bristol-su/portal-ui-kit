4594549aea262fd8a652130977da08fe8396cac8
Toby Twigger <tobytwigger1@gmail.com>
2021-08-17 14:31:36 +0100

    Update button to work with @click.prevent

--- src/components/atomic/VButton.vue
+++ src/components/atomic/VButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-      @click.prevent="triggerButton"
+      v-on="$listeners"
       type="submit"
       class="w-full px-16 py-2 my-2 mr-2 text-base text-white transition duration-500 ease-in-out transform bg-blue-600 border-blue-600 rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:bg-blue-800 "
     >
@@ -13,11 +13,6 @@ export default {
     name: "VButton",
     props: {
         buttonText: {type: String, required: false, default: 'Submit'}
-    },
-    methods: {
-        triggerButton() {
-            this.$emit('submit');
-        }
     }
 }
 </script>
