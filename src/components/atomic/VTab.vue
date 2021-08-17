7ffe2f8c30a109adbe4466ff095afa760c89d8db
Toby Twigger <tobytwigger1@gmail.com>
2021-08-17 01:12:37 +0100

    Add tabs

---
+++ src/components/atomic/VTab.vue
@@ -0,0 +1,32 @@
+<template>
+    <div v-show='isActive'>
+        <slot></slot>
+    </div>
+<!--    <div style='border-bottom: 2px solid #eaeaea'>-->
+<!--        <ul class='flex cursor-pointer'>-->
+<!--            <li class='py-2 px-6 bg-white rounded-t-lg'>Personal</li>-->
+<!--            <li class='py-2 px-6 bg-white rounded-t-lg text-gray-500 bg-gray-200'>Akun</li>-->
+<!--            <li class='py-2 px-6 bg-white rounded-t-lg text-gray-500 bg-gray-200'>Pengaturan</li>-->
+<!--        </ul>-->
+<!--    </div>-->
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            default: 'VTab'
+        }
+    },
+    data () {
+        return {
+            isActive: true
+        }
+    }
+}
+</script>
+
+<style lang="css">
+
+</style>
---
+++ src/components/atomic/VTabs.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <div style="border-bottom: 2px solid #eaeaea">
+            <ul class="flex cursor-pointer">
+                <li class='py-2 px-6 bg-white rounded-t-lg'
+                    v-for='(tab, index) in tabs'
+                    :key='tab.title'
+                    @click='selectTab(index)'
+                    :class='{" text-gray-500 bg-gray-200": (index !== selectedIndex)}'
+                >
+                    {{ tab.title }}
+                </li>
+            </ul>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "VTabs",
+    data () {
+        return {
+            selectedIndex: 0, // the index of the selected tab,
+            tabs: []         // all of the tabs
+        }
+    },
+    created () {
+        this.tabs = this.$children
+    },
+    mounted () {
+        this.selectTab(0)
+    },
+    methods: {
+        selectTab (i) {
+            this.selectedIndex = i
+
+            // loop over all the tabs
+            this.tabs.forEach((tab, index) => {
+                tab.isActive = (index === i)
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
