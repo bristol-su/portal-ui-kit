da954bac9be0fb9e595787abf62a49c787ec6f3b
Toby Twigger <tobytwigger1@gmail.com>
2021-08-17 15:08:57 +0100

    Fix refs for the last time?

--- src/components/VSubmitForm.vue
+++ src/components/VSubmitForm.vue
@@ -43,7 +43,7 @@ export default {
     },
     methods: {
         checkValidity() {
-            if (!this.refs.$form.checkValidity()) {
+            if (!this.$refs.form.checkValidity()) {
                 e.preventDefault();
             }
         }
