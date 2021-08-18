7c67f6035f7758bac1a2d8a5f4d54eaf2875ea71
Toby Twigger <tobytwigger1@gmail.com>
2021-08-18 11:16:25 +0100

    Use file not value

--- src/components/atomic/dynamic-form/VFileUpload.vue
+++ src/components/atomic/dynamic-form/VFileUpload.vue
@@ -6,7 +6,7 @@
         <input
             :id="id"
             :name="name"
-            @change="setValue($event.target.value)"
+            @change="setValue($event.target.files)"
             tabindex="-1"
             type="file"
             class="w-full px-4 py-2 mt-2 text-base text-black transition duration-500 ease-in-out transform rounded-lg bg-blueGray-100 focus:border-blueGray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2">
