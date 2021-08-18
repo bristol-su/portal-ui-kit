68ce22d7e8c58b16d9d30e2d4754c6fa7ceacfda
Toby Twigger <tobytwigger1@gmail.com>
2021-08-18 13:54:34 +0100

    Update

--- src/components/atomic/VTabs.vue
+++ src/components/atomic/VTabs.vue
@@ -3,7 +3,7 @@
         <div style="border-bottom: 2px solid #eaeaea">
             <ul class="flex cursor-pointer">
                 <li class='py-2 px-6 bg-white rounded-t-lg'
-                    v-for='(tab, index) in getTabs()'
+                    v-for='(tab, index) in tabs'
                     :key='tab.title'
                     @click='selectTab(index)'
                     :class='{"text-gray-500 bg-gray-200": (index != selectedIndex)}'
