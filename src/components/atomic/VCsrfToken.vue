257b3d13fa94519df6d09327efb2e9f573cf090d
Toby Twigger <tobytwigger1@gmail.com>
2021-08-19 16:08:24 +0100

    Add CSRF token and edit top bar to match what we need

---
+++ src/components/atomic/VCsrfToken.vue
@@ -0,0 +1,18 @@
+<template>
+    <input type="hidden" name="_token" :value="csrf"/>
+</template>
+
+<script>
+import Csrf from './index';
+export default {
+    name: "VCsrfToken",
+    computed: {
+        csrf() {
+            return document.head.querySelector('meta[name="csrf-token"]').content;
+        },
+        hasCsrf() {
+            return !!document.head.querySelector('meta[name="csrf-token"]');
+        }
+    }
+}
+</script>
