d4c896f468cad277af11e6f2db995a951f003289
Toby Twigger <tobytwigger1@gmail.com>
2021-08-19 16:27:06 +0100

    Make the button fire events

--- src/components/atomic/VButton.vue
+++ src/components/atomic/VButton.vue
@@ -1,14 +1,14 @@
 <template>
     <a
-      :href=href
+      @click.prevent="clickButton"
       v-on="$listeners"
       type="submit"
       class="w-full px-16 py-2 my-2 mr-2 text-base text-white transition duration-500 ease-in-out transform rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2"
       :class="styles"
     >
-      <slot>
-        {{ buttonText }}
-      </slot>
+        <slot>
+            {{ buttonText }}
+        </slot>
     </a>
 </template>
 
@@ -18,16 +18,24 @@ export default {
     props: {
         buttonText: {type: String, required: false, default: 'Submit'},
         href: { type: String, required: false },
-        type: { type: String, required: false, default: 'primary' },
+        type: { type: String, required: false, default: 'primary' }
     },
-  computed: {
-      styles() {
-        return {
-          'bg-blue-600 border-blue-600 hover:bg-blue-800': this.type === 'primary',
-          'bg-gray-600 border-gray-600 hover:bg-gray-800': this.type === 'secondary'
+    methods: {
+        clickButton() {
+            if(this.href) {
+                window.location.href = this.href;
+            }
+            this.$emit('click');
         }
-      }
-  }
+    },
+    computed: {
+        styles() {
+            return {
+                'bg-blue-600 border-blue-600 hover:bg-blue-800': this.type === 'primary',
+                'bg-gray-600 border-gray-600 hover:bg-gray-800': this.type === 'secondary'
+            }
+        }
+    }
 }
 </script>
 
