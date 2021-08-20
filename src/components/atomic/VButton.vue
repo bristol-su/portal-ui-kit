1371404aaade954c3c739e9cf2775406f323a7cf
Toby Twigger <tobytwigger1@gmail.com>
2021-08-20 12:39:39 +0100

    Change type to variant

--- src/components/atomic/VButton.vue
+++ src/components/atomic/VButton.vue
@@ -43,10 +43,10 @@ export default {
     computed: {
         styles() {
             return {
-                'bg-blue-600 border-blue-600 hover:bg-blue-800': this.type === 'primary',
-                'bg-gray-600 border-gray-600 hover:bg-gray-800': this.type === 'secondary',
-                'w-full px-16 py-2 my-2 text-base font-medium text-blueGray-600 transition duration-500 ease-in-out transform rounded-md border-blueGray-50 bg-blueGray-50 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:bg-blueGray-100': this.type === 'white',
-                'w-full px-16 py-2 my-2 text-base font-medium text-white transition duration-500 ease-in-out transform border-black rounded-md bg-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:bg-blueGray-900': this.type === 'black',
+                'bg-blue-600 border-blue-600 hover:bg-blue-800': this.variant === 'primary',
+                'bg-gray-600 border-gray-600 hover:bg-gray-800': this.variant === 'secondary',
+                'w-full px-16 py-2 my-2 text-base font-medium text-blueGray-600 transition duration-500 ease-in-out transform rounded-md border-blueGray-50 bg-blueGray-50 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:bg-blueGray-100': this.variant === 'white',
+                'w-full px-16 py-2 my-2 text-base font-medium text-white transition duration-500 ease-in-out transform border-black rounded-md bg-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:bg-blueGray-900': this.variant === 'black',
             }
         }
     }
