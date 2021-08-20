c5c6b59ff0c6067b386d97d7fef2171802728f4b
Toby Twigger <tobytwigger1@gmail.com>
2021-08-20 15:33:59 +0100

    Update layout props

--- src/components/layout/VLayout.vue
+++ src/components/layout/VLayout.vue
@@ -1,6 +1,6 @@
 <template>
-    <h1 class="mx-auto mb-12 text-2xl font-semibold leading-none tracking-tighter text-black lg:text-3xl title-font" v-if="pageTitle" v-text="pageTitle"></h1>
-    <h2 class="mx-auto mb-4 text-xl font-semibold leading-none tracking-tighter text-black title-font" v-if="pageSubtitle" v-text="pageSubtitle"></h2>
+    <h1 class="mx-auto mb-12 text-2xl font-semibold leading-none tracking-tighter text-black lg:text-3xl title-font" v-if="title" v-text="title"></h1>
+    <h2 class="mx-auto mb-4 text-xl font-semibold leading-none tracking-tighter text-black title-font" v-if="subtitle" v-text="subtitle"></h2>
     <div class="mx-auto text-base font-medium leading-relaxed text-blueGray-700">
         <slot></slot>
     </div>
@@ -10,8 +10,8 @@
 export default {
   name: "VLayout",
   props: {
-    pageTitle: { type: String, required: false },
-    pageSubtitle: { type: String, required: false }
+    title: { type: String, required: false },
+    subtitle: { type: String, required: false }
   }
 }
 </script>
