dc2789b6e187ae11b54a73abb46e598f74601c8b
Toby Twigger <tobytwigger1@gmail.com>
2021-08-20 12:34:01 +0100

    Add extra button variants

--- src/components/atomic/VButton.vue
+++ src/components/atomic/VButton.vue
@@ -30,7 +30,7 @@ export default {
     props: {
         buttonText: {type: String, required: false, default: 'Submit'},
         href: {type: String, required: false},
-        type: {type: String, required: false, default: 'primary'}
+        variant: {type: String, required: false, default: 'primary'}
     },
     methods: {
         clickButton() {
@@ -44,7 +44,9 @@ export default {
         styles() {
             return {
                 'bg-blue-600 border-blue-600 hover:bg-blue-800': this.type === 'primary',
-                'bg-gray-600 border-gray-600 hover:bg-gray-800': this.type === 'secondary'
+                'bg-gray-600 border-gray-600 hover:bg-gray-800': this.type === 'secondary',
+                'w-full px-16 py-2 my-2 text-base font-medium text-blueGray-600 transition duration-500 ease-in-out transform rounded-md border-blueGray-50 bg-blueGray-50 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:bg-blueGray-100': this.type === 'white',
+                'w-full px-16 py-2 my-2 text-base font-medium text-white transition duration-500 ease-in-out transform border-black rounded-md bg-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:bg-blueGray-900': this.type === 'black',
             }
         }
     }
