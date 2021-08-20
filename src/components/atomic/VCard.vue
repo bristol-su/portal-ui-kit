b18962fd7773f19834ff7edd47af86a458a237c0
Toby Twigger <tobytwigger1@gmail.com>
2021-08-20 15:41:08 +0100

    Update card prop name

--- src/components/atomic/VCard.vue
+++ src/components/atomic/VCard.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="w-full p-6 mx-auto lg:w-1/3">
     <div class="shadow-xl rounded-xl bg-blueGray-50">
-      <img class="object-cover object-center w-full lg:h-48 md:h-36 rounded-t-xl" v-if="img" :src="img" :alt="title + ' banner image'">
+      <img class="object-cover object-center w-full lg:h-48 md:h-36 rounded-t-xl" v-if="imageUrl" :src="imageUrl" :alt="title + ' banner imageUrl'">
       <div class="p-4 lg:p-8 bg-blueGray-50">
         <h1 class="mx-auto mb-4 text-2xl font-semibold leading-none tracking-tighter text-black lg:text-3xl title-font">{{ title }}</h1>
-        <h2 class="mb-8 text-xs font-semibold tracking-widest text-black uppercase title-font" v-if="subtext">{{ subtext }}</h2>
-        <a :href=url class="inline-flex items-center mt-auto font-semibold text-blue-600 lg:mb-0 hover:text-black " :title="'Continue ' + title"> {{ urlString }} </a>
+        <h2 class="mb-8 text-xs font-semibold tracking-widest text-black uppercase title-font" v-if="subtitle">{{ subtitle }}</h2>
+        <a :href=url class="inline-flex items-center mt-auto font-semibold text-blue-600 lg:mb-0 hover:text-black " :title="'Continue ' + title"> {{ urlText }} </a>
       </div>
       <div class="px-6 py-4 rounded-b-xl" :class="progressStyling" v-if="progress">
         <p class="inline-flex items-center mt-auto font-semibold text-gray-500 lg:mb-0 " title="Read Full Article"> Progress {{ progress }} Complete</p>
@@ -19,10 +19,10 @@ export default {
   name: "VCard",
   props: {
     title: { type: String, required: true },
-    subtext: { type: String, required: false },
-    img: { type: String, required: false },
+    subtitle: { type: String, required: false },
+    imageUrl: { type: String, required: false },
     url: { type: String, required: true },
-    urlString: { type: String, required: false, default: "Continue Task" },
+    urlText: { type: String, required: false, default: "Continue Task" },
     progress: { type: Number, required: false },
   },
   computed: {
