5b78b51c4f0aeffef81f86a6c39d75b3d5072338
Toby Twigger <tobytwigger1@gmail.com>
2021-08-23 15:02:14 +0100

    Add actions slot

--- src/components/layout/VPageContent.vue
+++ src/components/layout/VPageContent.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1 class="mx-auto mb-12 text-2xl font-semibold leading-none tracking-tighter text-black lg:text-3xl title-font" v-if="title" v-text="title"></h1>
+        <slot name="actions"></slot>
         <h2 class="mx-auto mb-4 text-xl font-semibold leading-none tracking-tighter text-black title-font" v-if="subtitle" v-text="subtitle"></h2>
         <div class="mx-auto text-base font-medium leading-relaxed text-blueGray-700">
             <slot></slot>
