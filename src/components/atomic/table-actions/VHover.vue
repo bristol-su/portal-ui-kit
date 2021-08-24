3836a899596077318975f9d174793c87d8fc9a6e
Toby Twigger <tobytwigger1@gmail.com>
2021-08-24 13:15:31 +0100

    Add hover

---
+++ src/components/atomic/table-actions/VHover.vue
@@ -0,0 +1,20 @@
+<template>
+    <span>
+        <span class="ml-1 hidden group-hover:block">
+            <slot name="onHover"></slot>
+        </span>
+        <span class="ml-1 block group-hover:hidden">
+            <slot></slot>
+        </span>
+    </span>
+</template>
+
+<script>
+export default {
+    name: "VHover"
+}
+</script>
+
+<style scoped>
+
+</style>
