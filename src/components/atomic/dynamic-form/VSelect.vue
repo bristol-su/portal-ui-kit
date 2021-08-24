fecd97a068143ab4c385bed0fe9efe8c0b50ea29
Toby Twigger <tobytwigger1@gmail.com>
2021-08-24 12:00:14 +0100

    Support multupke

--- src/components/atomic/dynamic-form/VSelect.vue
+++ src/components/atomic/dynamic-form/VSelect.vue
@@ -6,6 +6,7 @@
                   :id="id"
                   :name="title"
                   v-model="dynamicValue"
+                  :multiple="multiple"
                   class="w-full px-4 py-2 mt-2 text-base text-black transition duration-500 ease-in-out transform rounded-lg bg-blueGray-100 focus:border-blueGray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2">
                     <option value=""></option>
                     <option v-for="option in selectOptions" :value="option.id" v-text="option.value"></option>
@@ -26,6 +27,11 @@ export default {
             required: true,
             type: Array,
             validator: (val) => Array.isArray(val) && val.length > 0 && val.filter((option) => !option.hasOwnProperty('id') || !option.hasOwnProperty('value')).length === 0
+        },
+        multiple: {
+            required: false,
+            type: Boolean,
+            default: false
         }
     },
     mixins: [FormInputMixin]
