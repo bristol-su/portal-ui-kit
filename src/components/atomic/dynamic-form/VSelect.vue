48c7335d6b26c57b9d278df4b154403b5647c5f9
Toby Twigger <tobytwigger1@gmail.com>
2021-08-29 23:57:06 +0100

    Add support for grouping options in select

--- src/components/atomic/dynamic-form/VSelect.vue
+++ src/components/atomic/dynamic-form/VSelect.vue
@@ -8,8 +8,21 @@
                   v-model="dynamicValue"
                   :multiple="multiple"
                   class="w-full px-4 py-2 mt-2 text-base text-black transition duration-500 ease-in-out transform rounded-lg bg-blueGray-100 focus:border-blueGray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2">
-                    <option v-if="showNullOption" :value="nullValue">{{nullLabel}}</option>
-                    <option v-for="option in selectOptions" :value="option.id" v-text="option.value"></option>
+                    <option v-if="showNullOption" :value="nullValue">{{ nullLabel }}</option>
+                    <option
+                      v-for="option in ungroupedSelectOptions"
+                      :value="option.id"
+                      v-text="option.value"
+                    >
+                    </option>
+                    <optgroup v-for="(options, group) in groupedSelectOptions">
+                        <option
+                          v-for="option in options"
+                          :value="option.id"
+                          v-text="option.value"
+                        >
+                        </option>
+                    </optgroup>
                 </select>
             </div>
         </v-form-label>
@@ -22,6 +35,11 @@ import FormInputMixin from './FormInputMixin';
 
 export default {
     name: "VSelect",
+    data() {
+        return {
+            ungroupedKey: 'p-ungrouped-options'
+        }
+    },
     props: {
         selectOptions: {
             required: true,
@@ -46,6 +64,21 @@ export default {
             required: false,
             type: Boolean,
             default: false
+        }
+    },
+    computed: {
+        groupedSelectOptions() {
+            let groups = {};
+            let addOption = (group, option) => {
+                if (!groups.hasOwnProperty(option.group)) {
+                    groups[option.group] = [];
+                }
+                groups[option.group].push(option);
+            }
+            this.selectOptions.filter((option) => option.group).forEach((option) => addOption(option));
+        },
+        ungroupedSelectOptions() {
+            return this.selectOptions.filter((option) => !option.group);
         }
     },
     mixins: [FormInputMixin]
