bbfa91cf7800e82b5994858e90aab719cd9c5089
Aidan Laycock <aidanl_@hotmail.co.uk>
2021-08-30 14:58:33 +0100

    FormLabel: Added Icon for Tooltip

--- src/components/atomic/VFormLabel.vue
+++ src/components/atomic/VFormLabel.vue
@@ -3,7 +3,12 @@
     <label :content=tooltip :for=title class="text-base leading-7 text-blueGray-500" v-if="title"
            v-tippy="{ arrow: true, animation: 'fade', placement: 'top-start', arrow: true, interactive: true, onShow: () => !!tooltip }"
     >
-      {{ title }} <span v-if="required">* <span class="sr-only">Required</span></span>
+            <span class="inline-flex">{{ title }}
+            <span v-if="required"> * <span class="sr-only">Required</span></span>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor" v-if="hasTooltip">
+                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+            </svg>
+            </span>
     </label>
     <slot></slot>
     <v-help-text v-if="hasHelp" :help=help></v-help-text>
