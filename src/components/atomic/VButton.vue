859b5e7f0fda8baf6ee7f824c5555eb1219f1dc9
Aidan Laycock <aidanl_@hotmail.co.uk>
2021-08-30 16:50:57 +0100

    Updated Text on V Button in line with accessibility.

--- src/components/atomic/VButton.vue
+++ src/components/atomic/VButton.vue
@@ -3,7 +3,7 @@
         <a v-if="href" :href="href">
             <button
                 type="button"
-                class="w-full px-16 py-2 my-2 mr-2 text-base text-white transition duration-500 ease-in-out transform rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2"
+                class="w-full px-16 py-2 my-2 mr-2 text-base transition duration-500 ease-in-out transform rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2"
                 :class="styles"
                 :disabled="disabled"
             >
@@ -15,7 +15,7 @@
         <button v-else
                 v-on="$listeners"
                 type="submit"
-                class="w-full px-16 py-2 my-2 mr-2 text-base text-white transition duration-500 ease-in-out transform rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2"
+                class="w-full px-16 py-2 my-2 mr-2 text-base transition duration-500 ease-in-out transform rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2"
                 :class="styles"
                 :disabled="disabled"
         >
@@ -46,8 +46,8 @@ export default {
   computed: {
     styles() {
       return {
-        'bg-primary border-primary-light hover:bg-primary-dark': this.variant === 'primary',
-        'bg-secondary border-secondary-light hover:bg-secondary-dark': this.variant === 'secondary',
+        'bg-primary border-primary-light text-black hover:bg-primary-dark hover:text-white': this.variant === 'primary',
+        'bg-secondary border-secondary-light text-black hover:bg-secondary-dark hover:text-white': this.variant === 'secondary',
         'bg-success border-success-light hover:bg-success-dark': this.variant === 'success',
         'bg-warning border-warning-light hover:bg-warning-dark': this.variant === 'danger',
         'bg-info border-info-light hover:bg-info-dark': this.variant === 'info',
