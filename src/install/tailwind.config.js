752bda854c60e4fc7aac9e5f678f04f506ae475f
Aidan Laycock <aidanl_@hotmail.co.uk>
2021-08-30 15:19:22 +0100

    Added Bristol Branding Colours to Theme.

--- src/install/tailwind.config.js
+++ src/install/tailwind.config.js
@@ -2,7 +2,28 @@ module.exports = {
     purge: [],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        fontFamily: {},
+        extend: {
+            colors: {
+                primary: {
+                    light: '#689fea',
+                    DEFAULT: '#0984e3',
+                    dark: '#2452a1'
+                },
+                secondary: {
+                    light: '#4aecec',
+                    DEFAULT: '#2dbccf',
+                    dark: '#2893a1'
+                },
+                ternary: {
+                    light: '#55efc4',
+                    DEFAULT: '#16e497',
+                    dark: '#16c894'
+                },
+                black: '#262626',
+                white: '#fef6f6'
+            },
+        },
     },
     variants: {
         extend: {
