cd0b83cca8b1de4d4cd36506d350d798bce6e4a5
Toby Twigger <tobytwigger1@gmail.com>
2021-08-31 19:21:33 +0100

    Fix lodash not loading

--- src/components/VTable.vue
+++ src/components/VTable.vue
@@ -24,7 +24,7 @@
                         <td class="px-6 py-4 whitespace-nowrap" v-for="column in fullColumns">
                             <slot :name="'cell(' + column.key + ')'" v-bind:row="row" v-bind:column="column">
                                 <slot name="cell()" v-bind:row="row" v-bind:column="column">
-                                    <div class="text-sm text-gray-900">{{ _.get(row, column.key, '') }}</div>
+                                    <div class="text-sm text-gray-900">{{ dataGet(row, column.key) }}</div>
                                 </slot>
                             </slot>
                         </td>
@@ -100,6 +100,9 @@ export default {
             if (this.totalCount) {
                 this.$emit('changePage', {'page': this.page, 'size': this.pageSize});
             }
+        },
+        dataGet(row, key) {
+            return _.get(row, key, '')
         }
     },
     computed: {
