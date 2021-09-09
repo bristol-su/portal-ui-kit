51a696f8b8c4f9ededa6222096c265056d826ae2
Toby Twigger <tobytwigger1@gmail.com>
2021-09-09 15:48:34 +0100

    Fix error summary bullet points

--- src/components/atomic/VErrorSummary.vue
+++ src/components/atomic/VErrorSummary.vue
@@ -1,25 +1,23 @@
 <template>
     <div class="bg-warning border-l-4 rounded-t border-warning-dark text-black p-4 text-left" role="alert">
         <p class="font-bold">There are {{errorCount}} errors in the form.</p>
-        <div v-for="(errors, errorId) in errors">
-            <ul>
-                <li v-for="error in errors">
-                    <a class="mt-5" :href="'#' + errorId">{{getLabel(errorId)}}</a>
-                    <div v-if="errors.length > 1">
-                        <ul>
-                            <li v-for="e in error">{{e}}</li>
-                        </ul>
-                    </div>
-                    <div v-else>{{error}}</div>
-                </li>
-            </ul>
-        </div>
+        <ul>
+        <li v-for="(errors, errorId) in errors" :key="errorId">
+            <a class="mt-5" :href="'#' + errorId">{{ getLabel(errorId) }}:</a>
+            <div v-if="errors.length > 1">
+                <ul>
+                    <li v-for="e in error">{{ e }}</li>
+                </ul>
+            </div>
+            <span v-else>{{ error }}</span>
+        </li>
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
-    name: "VError",
+    name: "VErrorSummary",
     props: {
         errors: {type: Object, required: true},
         labels: {type: Object, required: false}
