7ba8765202f9bbdbd551b1d580fca9dd6f9207f2
Toby Twigger <tobytwigger1@gmail.com>
2021-09-09 16:03:09 +0100

    Fix error summary text

--- src/components/atomic/VErrorSummary.vue
+++ src/components/atomic/VErrorSummary.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="bg-warning border-l-4 rounded-t border-warning-dark text-black p-4 text-left" role="alert">
-        <p class="font-bold">There are {{errorCount}} errors in the form.</p>
+    <div class="bg-warning border-l-4 rounded-t border-warning-dark text-black p-4 text-left" role="alert" v-if="errorCount > 0">
+        <p class="font-bold" v-if="errorCount === 1">There is 1 error in the form.</p>
+        <p class="font-bold" v-else-if="errorCount > 1">There are {{errorCount}} errors in the form.</p>
         <ul>
         <li v-for="(fieldErrors, errorId) in errors" :key="errorId">
             <a class="mt-5" :href="'#' + errorId">{{ getLabel(errorId) }}:</a>
