54baf53f048ee6f67749a3c320525d0059c67e98
Aidan Laycock <aidanl_@hotmail.co.uk>
2021-09-10 17:43:50 +0100

    Updated Error Summary to make more obvious that it's a link

--- src/components/atomic/VErrorSummary.vue
+++ src/components/atomic/VErrorSummary.vue
@@ -1,19 +1,19 @@
 <template>
-    <div class="bg-warning border-l-4 rounded-t border-warning-dark text-black p-4 text-left" role="alert" v-if="errorCount > 0">
-        <p class="font-bold" v-if="errorCount === 1">There is 1 error in the form.</p>
-        <p class="font-bold" v-else-if="errorCount > 1">There are {{errorCount}} errors in the form.</p>
-        <ul>
-        <li v-for="(fieldErrors, errorId) in errors" :key="errorId">
-            <a class="mt-5" :href="'#' + errorId">{{ getLabel(errorId) }}:</a>
-            <div v-if="fieldErrors.length > 1">
-                <ul>
-                    <li v-for="error in fieldErrors">{{ error }}</li>
-                </ul>
-            </div>
-            <span v-else>{{ fieldErrors[0] }}</span>
-        </li>
-        </ul>
-    </div>
+  <div class="bg-warning border-l-4 rounded-t border-warning-dark text-black p-4 text-left" role="alert" v-if="errorCount > 0">
+    <p class="font-bold" v-if="errorCount === 1">There is 1 error in the form.</p>
+    <p class="font-bold" v-else-if="errorCount > 1">There are {{errorCount}} errors in the form.</p>
+    <ul>
+      <li v-for="(fieldErrors, errorId) in errors" :key="errorId">
+        <a class="mt-5 underline" :href="'#' + errorId">{{ getLabel(errorId) }}:</a>
+        <div v-if="fieldErrors.length > 1">
+          <ul>
+            <li v-for="error in fieldErrors">{{ error }}</li>
+          </ul>
+        </div>
+        <span v-else>{{ fieldErrors[0] }}</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
