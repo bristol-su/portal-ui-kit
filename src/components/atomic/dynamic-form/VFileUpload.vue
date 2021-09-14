baf07f58555148171602388cba35fd9366a46dc7
Toby Twigger <tobytwigger1@gmail.com>
2021-09-14 15:18:51 +0100

    Remove tab index from file input

--- src/components/atomic/dynamic-form/VFileUpload.vue
+++ src/components/atomic/dynamic-form/VFileUpload.vue
@@ -1,23 +1,21 @@
 <template>
-  <div>
-    <v-form-label v-bind="labelProps">
-      <div class="mt-1 flex rounded-md shadow-sm">
-          <!-- Tab index (untested): https://stackoverflow.com/a/59199276 -->
-        <input
-          :aria-describedby="ariaDescribedBy"
-          :id="id"
-            :name="name"
-            @change="setValue($event.target.files)"
-          :aria-invalid="isInvalid"
-          :required="required"
-          tabindex="-1"
-            type="file"
-            class="w-full px-4 py-2 mt-2 text-base text-black transition duration-500 ease-in-out transform rounded-lg bg-blueGray-100 focus:border-blueGray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
-          :class=componentStyling
-        >
-      </div>
-    </v-form-label>
-  </div>
+    <div>
+        <v-form-label v-bind="labelProps">
+            <div class="mt-1 flex rounded-md shadow-sm">
+                <input
+                    :aria-describedby="ariaDescribedBy"
+                    :id="id"
+                    :name="name"
+                    @change="setValue($event.target.files)"
+                    :aria-invalid="isInvalid"
+                    :required="required"
+                    type="file"
+                    class="w-full px-4 py-2 mt-2 text-base text-black transition duration-500 ease-in-out transform rounded-lg bg-blueGray-100 focus:border-blueGray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
+                    :class=componentStyling
+                >
+            </div>
+        </v-form-label>
+    </div>
 </template>
 
 <script>
