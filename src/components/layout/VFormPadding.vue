99a64d2380bf35734b8492e0bbfbaeb1e313eb0e
Toby Twigger <tobytwigger1@gmail.com>
2021-09-14 15:25:18 +0100

    Fix form padding on wider screens

--- src/components/layout/VFormPadding.vue
+++ src/components/layout/VFormPadding.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container items-center px-5 py-12 lg:px-20">
         <div
-          class="flex flex-col w-full p-10 px-8 pt-6 mx-auto my-6 mb-4 transition duration-500 ease-in-out transform bg-white border rounded-lg lg:w-1/2">
+          class="flex flex-col w-full p-10 px-8 pt-6 mx-auto my-6 mb-4 transition duration-500 ease-in-out transform bg-white border rounded-lg xl:w-1/2">
             <slot></slot>
         </div>
     </div>
