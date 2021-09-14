b50eab9832788633efc27c7ad3807639410daeae
Toby Twigger <tobytwigger1@gmail.com>
2021-09-14 15:38:06 +0100

    Default form to empty object

--- src/components/VApiForm.vue
+++ src/components/VApiForm.vue
@@ -71,7 +71,7 @@ export default {
             }
         },
         reset() {
-            this.formData = this.initialData;
+            this.formData = this.initialData ?? {};
         }
     }
 }
