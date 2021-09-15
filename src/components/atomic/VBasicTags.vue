98099870dfabe8b42d79a057a5c275cca3dfe147
Toby Twigger <tobytwigger1@gmail.com>
2021-09-15 20:57:14 +0100

    Trigger actions on keydown not keyip

--- src/components/atomic/VBasicTags.vue
+++ src/components/atomic/VBasicTags.vue
@@ -4,7 +4,7 @@
             <div v-for="(tag, index) in tags"
                  class="text-xs inline-flex items-center font-bold leading-sm px-3 py-1 bg-blue-200 text-blue-700 rounded-full" tabindex="0">
                 {{ tag }}
-                <div class="ml-1" @keyup.enter.prevent="$emit('delete', index)" @keyup.space.prevent="$emit('delete', index)" @click.prevent="$emit('delete', index)" role="button" tabindex="0">
+                <a href="#" class="ml-1" @keydown.enter.prevent="$emit('delete', index)" @keydown.space.prevent="$emit('delete', index)" @click.prevent="$emit('delete', index)" role="button">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
                          stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                          class="feather feather-x-circle">
@@ -13,7 +13,7 @@
                         <line x1="9" y1="9" x2="15" y2="15"></line>
                     </svg>
                     <span class="sr-only">{{ deleteText }}</span>
-                </div>
+                </a>
             </div>
         </div>
     </div>
--- src/components/atomic/dynamic-form/VTags.vue
+++ src/components/atomic/dynamic-form/VTags.vue
@@ -11,11 +11,11 @@
                 :id="id"
                 v-model="inputValue"
                 :required="required"
-                @keyup.enter.prevent="addTag"
+                @keydown.enter.prevent="addTag"
                 class="w-full px-4 py-2 mt-2 text-base text-black transition duration-500 ease-in-out transform rounded-lg bg-blueGray-100 focus:border-blueGray-500 focus:bg-white focus:outline-none focus:shadow-outline focus:ring-2 ring-offset-current ring-offset-2"
                 :class=componentStyling
             >
-            <div class="absolute top-4 right-3 text-primary" @click="addTag" tabindex="0" role="button" @keyup.space.prevent="addTag" @keyup.enter.prevent="addTag">
+            <div class="absolute top-4 right-3 text-primary" @click="addTag" tabindex="0" role="button" @keydown.space.prevent="addTag" @keydown.enter.prevent="addTag">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
