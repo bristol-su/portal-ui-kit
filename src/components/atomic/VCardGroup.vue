607c9845098d237790ae1346164f2a4ff06c0807
Toby Twigger <tobytwigger1@gmail.com>
2021-09-15 12:04:22 +0100

    Make card group title more reasnable

--- src/components/atomic/VCardGroup.vue
+++ src/components/atomic/VCardGroup.vue
@@ -3,7 +3,7 @@
     <div class="container items-center px-5 py-8 mx-auto">
       <div class="flex flex-wrap justify-center mb-12 bg-offWhite rounded-xl shadow-xl">
         <div class="w-full text-center mt-5">
-          <h2 v-if="title !== ''" class="mb-6 text-2xl font-semibold tracking-tighter text-black sm:text-5xl title-font" >{{title}}</h2>
+          <h3 v-if="title !== ''" class="mb-6 text-2xl font-semibold tracking-tighter text-black" >{{title}}</h3>
         </div>
         <slot></slot>
       </div>
