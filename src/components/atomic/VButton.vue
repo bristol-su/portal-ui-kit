724c0489f328c81fbbeaaa55b98463f1b287c370
Toby Twigger <tobytwigger1@gmail.com>
2021-09-17 02:35:48 +0100

    Make busy slot surround spinner

--- src/components/atomic/VButton.vue
+++ src/components/atomic/VButton.vue
@@ -5,26 +5,27 @@
            type="button"
            role="button"
            :class="styles"
-           class="inline-grid align-middle w-full px-16 py-2 my-2 mr-2 text-center transition duration-500 ease-in-out transform rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 bg-secondary border-secondary-light text-black hover:bg-secondary-dark hover:text-white"
+           class="inline-grid align-middle w-full px-16 py-2 my-2 mr-2 text-center transition duration-500 ease-in-out transform rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2"
            :aria-live="busy ? 'polite' : undefined"
            :aria-busy="busy"
            :disabled="disabled || busy"
         >
             <span>
-            <slot v-if="!busy">
-                {{ buttonText }}
-            </slot>
-            <div class="flex flex-nowrap align-middle" v-else>
-                <svg class="animate-spin h-4 w-4 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none"
-                     viewBox="0 0 24 24">
-                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                    <path class="opacity-75" fill="currentColor"
-                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                </svg>
-                <slot name="busyContent">
-                    {{ busyText }}
+                <slot v-if="!busy">
+                    {{ buttonText }}
                 </slot>
-            </div>
+                <slot name="busyContent" v-else>
+                    <div class="flex flex-nowrap align-middle">
+                        <svg class="animate-spin h-4 w-4 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none"
+                             viewBox="0 0 24 24">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                    stroke-width="4"></circle>
+                            <path class="opacity-75" fill="currentColor"
+                                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
+                        {{ busyText }}
+                    </div>
+                </slot>
             </span>
         </a>
         <button v-else
@@ -38,19 +39,20 @@
         >
             <span>
                 <slot v-if="!busy">
-                {{ buttonText }}
-            </slot>
-            <div class="flex flex-nowrap align-middle" v-else>
-                <svg class="animate-spin -ml-1 mr-3 h-5 w-5 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none"
-                     viewBox="0 0 24 24">
-                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                    <path class="opacity-75" fill="currentColor"
-                          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                </svg>
-                <slot name="busyContent">
-                    {{ busyText }}
+                    {{ buttonText }}
                 </slot>
-            </div>
+                <slot name="busyContent" v-else>
+                    <div class="flex flex-nowrap align-middle">
+                        <svg class="animate-spin -ml-1 mr-3 h-5 w-5 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none"
+                             viewBox="0 0 24 24">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                                    stroke-width="4"></circle>
+                            <path class="opacity-75" fill="currentColor"
+                                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                        </svg>
+                        {{ busyText }}
+                    </div>
+                </slot>
             </span>
         </button>
     </div>
