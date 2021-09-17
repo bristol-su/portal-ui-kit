adcaed5588617595ad140a028cdde9f0ab2751e0
Aidan Laycock <aidanl_@hotmail.co.uk>
2021-09-17 08:32:33 +0100

    Resolving Content placement when button busy.

--- src/components/atomic/VButton.vue
+++ src/components/atomic/VButton.vue
@@ -15,7 +15,7 @@
                     {{ buttonText }}
                 </slot>
                 <slot name="busyContent" v-else>
-                    <div class="flex flex-nowrap align-middle">
+                    <div class="flex justify-center">
                         <svg class="animate-spin h-4 w-4 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none"
                              viewBox="0 0 24 24">
                             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
@@ -42,7 +42,7 @@
                     {{ buttonText }}
                 </slot>
                 <slot name="busyContent" v-else>
-                    <div class="flex flex-nowrap align-middle">
+                    <div class="flex justify-center">
                         <svg class="animate-spin -ml-1 mr-3 h-5 w-5 mr-3" xmlns="http://www.w3.org/2000/svg" fill="none"
                              viewBox="0 0 24 24">
                             <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
