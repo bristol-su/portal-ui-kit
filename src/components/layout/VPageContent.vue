bdcc1c66aa4f054a641026381792d042a79d9d17
Toby Twigger <tobytwigger1@gmail.com>
2021-10-22 22:33:25 +0100

    Show HTML in page content, and underline the <a> tags

--- src/components/layout/VPageContent.vue
+++ src/components/layout/VPageContent.vue
@@ -2,7 +2,9 @@
     <div>
         <h1 class="mx-auto mb-12 text-2xl font-semibold leading-none tracking-normal text-black lg:text-3xl title-font" v-if="title" v-text="title"></h1>
         <slot name="actions"></slot>
-        <h2 class="mx-auto mb-4 text-xl font-semibold leading-none tracking-normal text-black title-font" v-if="subtitle" v-html="subtitle"></h2>
+        <h2 class="mx-auto mb-4 text-xl font-semibold leading-none tracking-normal text-black title-font" v-if="subtitle">
+            <v-html-renderer :html="subtitle"></v-html-renderer>
+        </h2>
         <div class="mx-auto text-base font-medium leading-relaxed text-blueGray-700">
             <slot></slot>
         </div>
@@ -10,12 +12,17 @@
 </template>
 
 <script>
+import VHtmlRenderer from './../atomic/VHtmlRenderer';
+
 export default {
   name: "VPageContent",
   props: {
     title: { type: String, required: false },
     subtitle: { type: String, required: false }
-  }
+  },
+    components: {
+        VHtmlRenderer
+    }
 }
 </script>
 
